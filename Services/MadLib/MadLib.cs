16878d103ed6acc81bab011fb72c3f9a7c93f431
Aiden Foo <aidenfoo154@gmail.com>
2024-01-10 13:28:30 -0800

    Making Madlib Make More sense

--- Services/MadLib/MadLib.cs
+++ Services/MadLib/MadLib.cs
@@ -4,6 +4,6 @@ public class MadLib : IMadLib
 {
     public string MadLibStory(string name, string place, string adverb, string name2, int numberOf, string thing, string adjective2, string verb, string verb2)
     {
-        return $"{name} went to {place} {adverb} in order to go see {name2}'s {numberOf} {thing}. They had a(n) {adjective2} time, {verb} and {verb2} all day long.";
+        return $"{name} went to {place} {adverb} in order to go see {name2}'s {numberOf} {thing}(s). They had a(n) {adjective2} time, {verb} and {verb2} all day long.";
     }
 }
